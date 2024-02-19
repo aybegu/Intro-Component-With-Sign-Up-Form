7c82a95cc779bd10f1115e4d5a2e2859d3a44d6d
Ayca Gurler <berfingurler@hotmail.com>
2024-02-19 17:26:57 +0300

    first commit: initializing

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <main>
+
+        Learn to code by watching others
+
+        See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+        but understanding how developers think is invaluable.
+
+        Try it free 7 days then $20/mo. thereafter
+
+        First Name
+        Last Name
+        Email Address
+        Password
+
+        Claim your free trial
+
+        By clicking the button, you are agreeing to our Terms and Services
+      </main>
+
+      <footer>
+        <p class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="https://github.com/aybegu">Ayca Berfin Gurler</a>.
+        </p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
